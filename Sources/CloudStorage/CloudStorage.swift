4b8f6a7e969c258bc993132ee2b3c45ca13613e5
Tom Lokhorst <tom@lokhorst.eu>
2023-04-18 09:03:27 +0200

    Refactor internals

--- Sources/CloudStorage/CloudStorage.swift
+++ Sources/CloudStorage/CloudStorage.swift
@@ -6,175 +6,144 @@
 //
 
 import SwiftUI
+import Combine
 
 private let sync = CloudStorageSync.shared
 
 @propertyWrapper public struct CloudStorage<Value>: DynamicProperty {
-    private let _setValue: (Value) -> Void
-
-    @ObservedObject private var backingObject: CloudStorageBackingObject<Value>
-
-    public var projectedValue: Binding<Value>
+    @ObservedObject private var object: CloudStorageObject<Value>
 
     public var wrappedValue: Value {
-        get { backingObject.value }
-        nonmutating set { _setValue(newValue) }
+        get { object.value }
+        nonmutating set { object.value = newValue }
     }
 
-    public init(keyName key: String, syncGet: @escaping () -> Value, syncSet: @escaping (Value) -> Void) {
-        let value = syncGet()
+    public var projectedValue: Binding<Value> {
+        Binding { object.value } set: { object.value = $0 }
+    }
 
-        let backing = CloudStorageBackingObject(value: value)
-        self.backingObject = backing
-        self.projectedValue = Binding(
-            get: { backing.value },
-            set: { newValue in
-                backing.value = newValue
-                syncSet(newValue)
-                sync.synchronize()
-            })
-        self._setValue = { (newValue: Value) in
-            backing.value = newValue
-            syncSet(newValue)
-            sync.synchronize()
-        }
-
-        sync.setObserver(for: key) { [weak backing] in
-            backing?.value = syncGet()
-        }
+    public init(keyName key: String, syncBinding: Binding<Value>) {
+        self.object = CloudStorageObject(key: key, syncBinding: syncBinding)
     }
 }
 
-internal class CloudStorageBackingObject<Value>: ObservableObject {
-    @Published var value: Value
+internal class CloudStorageObject<Value>: ObservableObject {
+    private let syncBinding: Binding<Value>
 
-    init(value: Value) {
-        self.value = value
+    var value: Value {
+        get { syncBinding.wrappedValue }
+        set {
+            syncBinding.wrappedValue = newValue
+            sync.synchronize()
+            objectWillChange.send()
+        }
+    }
+
+    init(key: String, syncBinding: Binding<Value>) {
+        self.syncBinding = syncBinding
+
+        sync.addObserver(for: key, publisher: objectWillChange)
+    }
+
+    deinit {
+        sync.removeObserver(publisher: objectWillChange)
     }
 }
 
 extension CloudStorage where Value == Bool {
     public init(wrappedValue: Value, _ key: String) {
-        self.init(
-            keyName: key,
-            syncGet: { sync.bool(for: key) ?? wrappedValue },
-            syncSet: { newValue in sync.set(newValue, for: key) })
+        let binding = Binding { sync.bool(for: key) ?? wrappedValue } set: { sync.set($0, for: key) }
+        self.init(keyName: key, syncBinding: binding)
     }
 }
 
 extension CloudStorage where Value == Int {
     public init(wrappedValue: Value, _ key: String) {
-        self.init(
-            keyName: key,
-            syncGet: { sync.int(for: key) ?? wrappedValue },
-            syncSet: { newValue in sync.set(newValue, for: key) })
+        let binding = Binding { sync.int(for: key) ?? wrappedValue } set: { sync.set($0, for: key) }
+        self.init(keyName: key, syncBinding: binding)
     }
 }
 
 extension CloudStorage where Value == Double {
     public init(wrappedValue: Value, _ key: String) {
-        self.init(
-            keyName: key,
-            syncGet: { sync.double(for: key) ?? wrappedValue },
-            syncSet: { newValue in sync.set(newValue, for: key) })
+        let binding = Binding { sync.double(for: key) ?? wrappedValue } set: { sync.set($0, for: key) }
+        self.init(keyName: key, syncBinding: binding)
     }
 }
 
 extension CloudStorage where Value == String {
     public init(wrappedValue: Value, _ key: String) {
-        self.init(
-            keyName: key,
-            syncGet: { sync.string(for: key) ?? wrappedValue },
-            syncSet: { newValue in sync.set(newValue, for: key) })
+        let binding = Binding { sync.string(for: key) ?? wrappedValue } set: { sync.set($0, for: key) }
+        self.init(keyName: key, syncBinding: binding)
     }
 }
 
 extension CloudStorage where Value == URL {
     public init(wrappedValue: Value, _ key: String) {
-        self.init(
-            keyName: key,
-            syncGet: { sync.string(for: key).flatMap(URL.init(string:)) ?? wrappedValue },
-            syncSet: { newValue in sync.set(newValue.absoluteString, for: key) })
+        let binding = Binding { sync.url(for: key) ?? wrappedValue } set: { sync.set($0, for: key) }
+        self.init(keyName: key, syncBinding: binding)
     }
 }
 
 extension CloudStorage where Value == Data {
     public init(wrappedValue: Value, _ key: String) {
-        self.init(
-            keyName: key,
-            syncGet: { sync.data(for: key) ?? wrappedValue },
-            syncSet: { newValue in sync.set(newValue, for: key) })
+        let binding = Binding { sync.data(for: key) ?? wrappedValue } set: { sync.set($0, for: key) }
+        self.init(keyName: key, syncBinding: binding)
     }
 }
 
 extension CloudStorage where Value: RawRepresentable, Value.RawValue == Int {
     public init(wrappedValue: Value, _ key: String) {
-        self.init(
-            keyName: key,
-            syncGet: { sync.int(for: key).flatMap(Value.init) ?? wrappedValue },
-            syncSet: { newValue in sync.set(newValue.rawValue, for: key) })
+        let binding = Binding { sync.int(for: key).flatMap(Value.init) ?? wrappedValue } set: { sync.set($0.rawValue, for: key) }
+        self.init(keyName: key, syncBinding: binding)
     }
 }
 
 extension CloudStorage where Value: RawRepresentable, Value.RawValue == String {
     public init(wrappedValue: Value, _ key: String) {
-        self.init(
-            keyName: key,
-            syncGet: { sync.string(for: key).flatMap(Value.init) ?? wrappedValue },
-            syncSet: { newValue in sync.set(newValue.rawValue, for: key) })
+        let binding = Binding { sync.string(for: key).flatMap(Value.init) ?? wrappedValue } set: { sync.set($0.rawValue, for: key) }
+        self.init(keyName: key, syncBinding: binding)
     }
 }
 
 extension CloudStorage where Value == Bool? {
     public init(_ key: String) {
-        self.init(
-            keyName: key,
-            syncGet: { sync.bool(for: key) },
-            syncSet: { newValue in sync.set(newValue, for: key) })
+        let binding = Binding { sync.bool(for: key) } set: { sync.set($0, for: key) }
+        self.init(keyName: key, syncBinding: binding)
     }
 }
 
 extension CloudStorage where Value == Int? {
     public init(_ key: String) {
-        self.init(
-            keyName: key,
-            syncGet: { sync.int(for: key) },
-            syncSet: { newValue in sync.set(newValue, for: key) })
+        let binding = Binding { sync.int(for: key) } set: { sync.set($0, for: key) }
+        self.init(keyName: key, syncBinding: binding)
     }
 }
 
 extension CloudStorage where Value == Double? {
     public init(_ key: String) {
-        self.init(
-            keyName: key,
-            syncGet: { sync.double(for: key) },
-            syncSet: { newValue in sync.set(newValue, for: key) })
+        let binding = Binding { sync.double(for: key) } set: { sync.set($0, for: key) }
+        self.init(keyName: key, syncBinding: binding)
     }
 }
 
 extension CloudStorage where Value == String? {
     public init(_ key: String) {
-        self.init(
-            keyName: key,
-            syncGet: { sync.string(for: key) },
-            syncSet: { newValue in sync.set(newValue, for: key) })
+        let binding = Binding { sync.string(for: key) } set: { sync.set($0, for: key) }
+        self.init(keyName: key, syncBinding: binding)
     }
 }
 
 extension CloudStorage where Value == URL? {
     public init(_ key: String) {
-        self.init(
-            keyName: key,
-            syncGet: { sync.string(for: key).flatMap(URL.init(string:)) },
-            syncSet: { newValue in sync.set(newValue?.absoluteString, for: key) })
+        let binding = Binding { sync.url(for: key) } set: { sync.set($0, for: key) }
+        self.init(keyName: key, syncBinding: binding)
     }
 }
 
 extension CloudStorage where Value == Data? {
     public init(_ key: String) {
-        self.init(
-            keyName: key,
-            syncGet: { sync.data(for: key) },
-            syncSet: { newValue in sync.set(newValue, for: key) })
+        let binding = Binding { sync.data(for: key) } set: { sync.set($0, for: key) }
+        self.init(keyName: key, syncBinding: binding)
     }
 }
